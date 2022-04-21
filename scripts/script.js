e12939937ac11ce235771328b6ba65eded474609
Amir Mardan <46511946+AmirMardan@users.noreply.github.com>
2022-04-21 16:36:04 -0400

    deleting old version

---
+++ scripts/script.js
@@ -0,0 +1,17 @@
+$(function () {
+    $(window).on('scroll', function () {
+        if ( $(window).scrollTop() > 10 ) {
+            $('.navbar').addClass('active');
+        } else {
+            $('.navbar').removeClass('active');
+        }
+    });
+});
+
+
+// JS written by Themetide
+
+
+
+
+
